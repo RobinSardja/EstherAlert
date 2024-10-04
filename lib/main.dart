4a2ee75120a3e8253689f777f746d660bc0a04ea
RobinSardja <robinsardja@gmail.com>
2024-10-04 16:08:30 -0400

    Designed basic layout.

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,70 @@ void main() {
     runApp( const MainApp() );
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
     const MainApp({super.key});
 
     @override
+    State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+
+    int currentIndex = 1;
+    final pageController = PageController(
+        initialPage: 1
+    );
+
+    @override
     Widget build( BuildContext context ) {
-        return const MaterialApp(
-            home: Scaffold(
-                body: Center(
-                    child: Text( "EtherAlert" )
+        return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+                appBarTheme: const AppBarTheme(
+                    backgroundColor: Colors.green,
+                    centerTitle: true,
+                    foregroundColor: Colors.yellow
                 ),
+                bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+                    backgroundColor: Colors.green,
+                    selectedItemColor: Colors.yellow
+                )
             ),
+            home: Scaffold(
+                appBar: AppBar(
+                    title: const Text( "EstherAlert" )
+                ),
+                body: PageView(
+                    controller: pageController,
+                    onPageChanged: (currentPage) => setState( () => currentIndex = currentPage ),
+                    children: const [
+                        Center( child: Text( "Account" ) ),
+                        Center( child: Text( "Home" ) ),
+                        Center( child: Text( "Settings" ) )
+                    ],
+                ),
+                bottomNavigationBar: BottomNavigationBar(
+                    currentIndex: currentIndex,
+                    onTap: (tappedIndex) {
+                        setState( () => currentIndex = tappedIndex );
+                        pageController.jumpToPage(tappedIndex);
+                    },
+                    items: const [
+                        BottomNavigationBarItem(
+                            icon: Icon( Icons.person ),
+                            label: "Account"
+                        ),
+                        BottomNavigationBarItem(
+                            icon: Icon( Icons.home ),
+                            label: "Home"
+                        ),
+                        BottomNavigationBarItem(
+                            icon: Icon( Icons.settings ),
+                            label: "Settings"
+                        )
+                    ],
+                ),
+            )
         );
     }
 }
